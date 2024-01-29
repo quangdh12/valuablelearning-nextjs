cea0b93ff3966aae31489fefd8770a46a4117c26
quangdh12 <quangdh.t63@gmail.com>
2024-01-29 17:36:13 +0700

    bugfix search courses

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -28,6 +28,8 @@ export const SearchInput = () => {
 			},
 			{ skipEmptyString: true, skipNull: true },
 		)
+
+		router.push(url)
 	}, [debounceValue, currentCategoryId, router, pathname])
 
 	return (
@@ -35,6 +37,7 @@ export const SearchInput = () => {
 			<Search className='h-4 w-4 absolute top-3 left-3 text-slate-600' />
 			<Input
 				onChange={(e) => setValue(e.target.value)}
+				value={value}
 				className='w-full md:w-[300px] pl-9 rounded-full 
                 bg-slate-100 focus-visible:ring-slate-200'
 				placeholder='Search for a course'
